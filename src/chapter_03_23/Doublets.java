4ff2db84a0c5237ff333cabeba0ebbe5ebf74b80
Minjae Kim <pomtech0410@gmail.com>
2013-08-21 00:46:55 +0900

    add question 23

---
+++ src/chapter_03_23/Doublets.java
@@ -0,0 +1,27 @@
+package chapter_03_23;
+
+public class Doublets {
+/*
+  
+ booster
+ rooster
+ roaster
+ coaster
+ roasted
+ coastal
+ postal
+ 
+ booster roasted
+ coastal postal
+ 
+ 
+ 
+ booster
+ rooster
+ roaster
+ roasted
+ 
+ No solution.
+  
+ * */
+}
